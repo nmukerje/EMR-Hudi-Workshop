9ab149d0635c52b849ddf09d2311c920a25ee073
Akshaya Rawat <rawaaksh@amazon.com>
2021-11-18 12:23:24 +0530

    Structured Spark Streaming consumer with HUDI

---
+++ spark-streaming/build.sbt
@@ -0,0 +1,31 @@
+name := "Spark-Structured-Streaming-Examples"
+
+version := "1.0"
+scalaVersion := "2.11.12"
+val sparkVersion = "2.4.5"
+val scala_tool_version="2.11"
+
+libraryDependencies += "log4j" % "log4j" % "1.2.14"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion
+libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % sparkVersion
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % sparkVersion
+libraryDependencies += "org.apache.hudi" % "hudi-spark-bundle_2.11" % "0.9.0"
+
+fork in run := true
+
+libraryDependencies += "org.elasticsearch" % "elasticsearch-hadoop" % "6.3.0"
+assemblyMergeStrategy in assembly := {
+ case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+ case x => MergeStrategy.first
+}
+
+assemblyExcludedJars in assembly := {
+    val cp = (fullClasspath in assembly).value
+    cp filter { f =>
+      f.data.getName.contains("spark-core") ||
+      f.data.getName.contains("hudi-spark-bundle_2.11")||
+      f.data.getName.contains("spark-streaming-kafka-0-10_2.11")
+    }
+  }
---
+++ spark-streaming/src/main/scala/kafka/hudi/SparkKafkaConsumerHudiProcessorCOW.scala
@@ -0,0 +1,72 @@
+package kafka.hudi
+
+import org.apache.hudi.config.HoodieWriteConfig
+import org.apache.hudi.hive.MultiPartKeysValueExtractor
+import org.apache.hudi.DataSourceWriteOptions
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{col,from_json}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode}
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.{DataFrame, Row, SaveMode}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+
+object SparkKafkaConsumerHudiProcessorCOW {
+
+  def main(args: Array[String]): Unit = {
+    
+    val spark = SparkSession
+          .builder
+          .appName("SparkHudi")
+          //.master("local[*]")
+          .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+          .config("spark.default.parallelism", 9)
+          .config("spark.sql.shuffle.partitions", 9)
+          .enableHiveSupport()
+          .getOrCreate()
+    
+    import spark.implicits._
+
+    val s3_bucket=args(0)
+    val kafkaBootstrap=args(1)
+    val topics = Array(args(2))
+    val dataPath=s"s3://$s3_bucket/dms-full-load-path/salesdb/SALES_ORDER_DETAIL/LOAD*"
+    val hudiTableName = "sales_order_detail_hudi_cow"
+    val hudiTableRecordKey = "record_key"
+    val hudiTablePartitionKey = "partition_key"
+    val hudiTablePrecombineKey = "order_date"
+    val hudiTablePath = s"s3://$s3_bucket/hudi/" + hudiTableName
+    val hudiHiveTablePartitionKey = "year,month"
+    val checkpoint_path=s"s3://$s3_bucket/hudi-checkpoint/"
+
+  
+    val streamingInputDF = spark.readStream.format("kafka").option("kafka.bootstrap.servers", kafkaBootstrap).option("subscribe", "s3_event_stream").option("startingOffsets", "earliest").load()
+
+    val schema= StructType(Array(StructField("filePath",StringType,true)))
+    val jsonDF=streamingInputDF.selectExpr("CAST(value AS STRING)").withColumn("jsonData",from_json(col("value"),schema)).select(col("jsonData.filePath"))
+
+    val query = jsonDF.writeStream.foreachBatch{ (batchDF: DataFrame, _: Long) => {
+            print(batchDF)
+            batchDF.collect().foreach( s=> { 
+                print(s)
+                var parDF=spark.read.format("parquet").load(s.getString(0));
+                parDF=parDF.drop("Op")
+                parDF=parDF.select(parDF.columns.map(x => col(x).as(x.toLowerCase)): _*)
+                parDF = parDF.withColumn(hudiTableRecordKey, concat(col("order_id"), lit("#"), col("line_id")))
+                parDF = parDF.withColumn("order_date", parDF("order_date").cast(DateType))
+                parDF = parDF.withColumn("year",year($"order_date").cast(StringType)).withColumn("month",month($"order_date").cast(StringType))
+                parDF = parDF.withColumn(hudiTablePartitionKey,concat(lit("year="),$"year",lit("/month="),$"month"))
+                parDF.printSchema()
+                parDF.select("record_key","quantity").show()
+                parDF.write.format("org.apache.hudi").option("hoodie.datasource.write.table.type", DataSourceWriteOptions.COW_STORAGE_TYPE_OPT_VAL).option(DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY, hudiTableRecordKey).option(DataSourceWriteOptions.PARTITIONPATH_FIELD_OPT_KEY, hudiTablePartitionKey).option(HoodieWriteConfig.TABLE_NAME, hudiTableName).option(DataSourceWriteOptions.OPERATION_OPT_KEY, DataSourceWriteOptions.UPSERT_OPERATION_OPT_VAL).option(DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY, hudiTablePrecombineKey).option(DataSourceWriteOptions.HIVE_SYNC_ENABLED_OPT_KEY, "true").option(DataSourceWriteOptions.HIVE_TABLE_OPT_KEY,hudiTableName).option(DataSourceWriteOptions.HIVE_PARTITION_FIELDS_OPT_KEY, hudiHiveTablePartitionKey).option("hoodie.datasource.hive_sync.assume_date_partitioning", "false").option(DataSourceWriteOptions.HIVE_PARTITION_EXTRACTOR_CLASS_OPT_KEY, classOf[MultiPartKeysValueExtractor].getName).mode("append").save(hudiTablePath);
+            })
+    }}.option("checkpointLocation", checkpoint_path).start()
+   
+
+    query.awaitTermination()
+
+  }
+
+}
